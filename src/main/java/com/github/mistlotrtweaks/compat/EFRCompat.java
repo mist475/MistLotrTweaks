9feaa1868d9287121a2cb26a08373ac76d3b5af3
Mist475 <475misterymob@gmail.com>
2024-07-15 16:18:08 +0200

    incorporate efr feedback

--- src/main/java/com/github/mistlotrtweaks/compat/EFRCompat.java
+++ src/main/java/com/github/mistlotrtweaks/compat/EFRCompat.java
@@ -2,7 +2,7 @@ package com.github.mistlotrtweaks.compat;
 
 import com.github.mistlotrtweaks.MistLotrTweaks;
 import com.github.mistlotrtweaks.config.MistLotrTweaksBeeHiveConfig;
-import ganymedes01.etfuturum.ModItems;
+import cpw.mods.fml.common.registry.GameRegistry;
 import ganymedes01.etfuturum.api.BeePlantRegistry;
 import ganymedes01.etfuturum.configuration.configs.ConfigBlocksItems;
 import ganymedes01.etfuturum.configuration.configs.ConfigEntities;
@@ -61,52 +61,6 @@ public class EFRCompat {
         BeePlantRegistry.addFlower(LOTRMod.doubleFlower, 1);
         BeePlantRegistry.addFlower(LOTRMod.doubleFlower, 2);
         BeePlantRegistry.addFlower(LOTRMod.doubleFlower, 3);
-        BeePlantRegistry.addFlower(LOTRMod.fruitLeaves, OreDictionary.WILDCARD_VALUE);
-        // Lemon
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 1);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 5);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 9);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 13);
-        // Orange
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 2);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 6);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 10);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 14);
-        // Lime
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 3);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 7);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 11);
-        BeePlantRegistry.addFlower(LOTRMod.leaves5, 15);
-        // Mallorn
-        BeePlantRegistry.addFlower(LOTRMod.leaves, 1);
-        BeePlantRegistry.addFlower(LOTRMod.leaves, 5);
-        BeePlantRegistry.addFlower(LOTRMod.leaves, 9);
-        BeePlantRegistry.addFlower(LOTRMod.leaves, 13);
-        // Chestnut
-        BeePlantRegistry.addFlower(LOTRMod.leaves4, 0);
-        BeePlantRegistry.addFlower(LOTRMod.leaves4, 4);
-        BeePlantRegistry.addFlower(LOTRMod.leaves4, 8);
-        BeePlantRegistry.addFlower(LOTRMod.leaves4, 12);
-        // Olive
-        BeePlantRegistry.addFlower(LOTRMod.leaves6, 3);
-        BeePlantRegistry.addFlower(LOTRMod.leaves6, 7);
-        BeePlantRegistry.addFlower(LOTRMod.leaves6, 11);
-        BeePlantRegistry.addFlower(LOTRMod.leaves6, 15);
-        // Almond
-        BeePlantRegistry.addFlower(LOTRMod.leaves7, 3);
-        BeePlantRegistry.addFlower(LOTRMod.leaves7, 7);
-        BeePlantRegistry.addFlower(LOTRMod.leaves7, 11);
-        BeePlantRegistry.addFlower(LOTRMod.leaves7, 15);
-        // Plum
-        BeePlantRegistry.addFlower(LOTRMod.leaves8, 0);
-        BeePlantRegistry.addFlower(LOTRMod.leaves8, 4);
-        BeePlantRegistry.addFlower(LOTRMod.leaves8, 8);
-        BeePlantRegistry.addFlower(LOTRMod.leaves8, 12);
-        // Pomegranate
-        BeePlantRegistry.addFlower(LOTRMod.leaves8, 2);
-        BeePlantRegistry.addFlower(LOTRMod.leaves8, 6);
-        BeePlantRegistry.addFlower(LOTRMod.leaves8, 10);
-        BeePlantRegistry.addFlower(LOTRMod.leaves8, 14);
 
         BeePlantRegistry.addFlower(LOTRMod.haradFlower, OreDictionary.WILDCARD_VALUE);
         BeePlantRegistry.addFlower(LOTRMod.morgulFlower, OreDictionary.WILDCARD_VALUE);
@@ -139,14 +93,16 @@ public class EFRCompat {
                 .filter(recipe -> !LOTRMod.mugMead.equals(recipe.getRecipeOutput().getItem()))
                 .collect(Collectors.toCollection(ArrayList::new));
 
+            var honeyBottle = GameRegistry.findItem("etfuturum", "honey_bottle");
+
             adjusted.add(new ShapelessOreRecipe(
                 new ItemStack(LOTRMod.mugMead, LOTRBrewingRecipes.BARREL_CAPACITY),
-                ModItems.HONEY_BOTTLE.get(),
-                ModItems.HONEY_BOTTLE.get(),
-                ModItems.HONEY_BOTTLE.get(),
-                ModItems.HONEY_BOTTLE.get(),
-                ModItems.HONEY_BOTTLE.get(),
-                ModItems.HONEY_BOTTLE.get())
+                honeyBottle,
+                honeyBottle,
+                honeyBottle,
+                honeyBottle,
+                honeyBottle,
+                honeyBottle)
             );
 
             lotrBrewingRecipes.set(null, adjusted);
